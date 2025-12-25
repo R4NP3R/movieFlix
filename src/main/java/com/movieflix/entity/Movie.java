a1bc07c11bd9159836fedf6c700bb7b0052da2b2
R4NP3R <ssrafa.2014@gmail.com>
2025-12-25 12:39:50 -0300

    feat: cria entidade, migration e repository do movie

---
+++ src/main/java/com/movieflix/entity/Movie.java
@@ -0,0 +1,38 @@
+package com.movieflix.entity;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  private String description;
+
+  @Column(name = "release_date")
+  private LocalDate releaseDate;
+
+  private Float rating;
+
+  @CreationTimestamp
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+}
