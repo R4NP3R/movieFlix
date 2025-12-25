081d19aa59ddc9da81ea9b2e2210c39779ff4afd
R4NP3R <ssrafa.2014@gmail.com>
2025-12-25 11:51:37 -0300

    feat: cria a entidade, migration e repository da category

---
+++ src/main/java/entity/Category.java
@@ -0,0 +1,24 @@
+package entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String name;
+}
