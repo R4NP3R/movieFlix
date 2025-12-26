484f403a8ce85abb253a34709007abd65d61f9c0
R4NP3R <ssrafa.2014@gmail.com>
2025-12-26 18:41:06 -0300

    feat: adiciona metodo de listar e deletar na controller e service do streaming

--- src/main/java/com/movieflix/controller/StreamingController.java
+++ src/main/java/com/movieflix/controller/StreamingController.java
@@ -2,15 +2,17 @@ package com.movieflix.controller;
 
 import com.movieflix.controller.request.StreamingRequest;
 import com.movieflix.controller.response.StreamingResponse;
+import com.movieflix.entity.Streaming;
 import com.movieflix.mapper.StreamingMapper;
 import com.movieflix.service.StreamingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieflix/streaming")
@@ -25,6 +27,38 @@ public class StreamingController {
     StreamingResponse newStreaming = StreamingMapper.map(streamingService.saveStreaming(request));
 
     return ResponseEntity.status(HttpStatus.CREATED).body(newStreaming);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<StreamingResponse> getStreamingById(@PathVariable Long id) {
+
+    return streamingService.getStreamingById(id)
+            .map(s -> ResponseEntity.ok(StreamingMapper.map(s)))
+            .orElse(ResponseEntity.notFound().build());
+  }
+
+  @GetMapping
+  public ResponseEntity<List<StreamingResponse>> listAllStreamings() {
+    List<StreamingResponse> streamings = new ArrayList<>();
+
+    streamingService.listAllStreaming().forEach(streaming -> streamings.add(StreamingMapper.map(streaming)));
+
+    return ResponseEntity.ok(streamings);
 
   }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteStreamingById(@PathVariable Long id) {
+    Optional<Streaming> optStreaming = streamingService.getStreamingById(id);
+
+    if (optStreaming.isPresent()) {
+      streamingService.deleteStreamingById(id);
+
+      return ResponseEntity.noContent().build();
+    }
+
+    return ResponseEntity.notFound().build();
+  }
+
+
 }
--- src/main/java/com/movieflix/service/StreamingService.java
+++ src/main/java/com/movieflix/service/StreamingService.java
@@ -7,6 +7,7 @@ import com.movieflix.repository.StreamingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,5 +26,14 @@ public class StreamingService {
   public Optional<Streaming> getStreamingById(Long id) {
     return streamingRepository.findById(id);
   }
+
+  public List<Streaming> listAllStreaming() {
+    return streamingRepository.findAll();
+  }
+
+  public void deleteStreamingById(Long id) {
+    streamingRepository.deleteById(id);
+  }
+
 
 }
