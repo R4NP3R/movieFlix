8caa005c2f85c12d1d1d750ef62e66a8148a11a5
R4NP3R <ssrafa.2014@gmail.com>
2025-12-26 10:09:02 -0300

    chore: adicionando anotações na entidade movie para leitura e escrita de dados

--- src/main/java/com/movieflix/entity/Movie.java
+++ src/main/java/com/movieflix/entity/Movie.java
@@ -2,6 +2,10 @@ package com.movieflix.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -11,6 +15,10 @@ import java.util.Date;
 
 @Entity
 @Table(name = "movie")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Movie {
 
   @Id
