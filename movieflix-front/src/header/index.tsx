7e443e92b28a2d56aad9ae40b0d244722183f89d
R4NP3R <ssrafa.2014@gmail.com>
2026-01-04 12:37:49 -0300

    feat: adiciona dropdown de opções de usuário ao header

--- movieflix-front/src/header/index.tsx
+++ movieflix-front/src/header/index.tsx
@@ -1,4 +1,9 @@
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { useState } from "react";
+
 export const Header = () => {
+  const [handleUserMenu, setHandleUserMenu] = useState(false);
+
   return (
     <header className="w-full fixed mt-">
       <div className="flex justify-between items-center my-container">
@@ -6,21 +11,54 @@ export const Header = () => {
         <nav className="py-6 px-8 bg-slate-800 rounded-b-4xl">
           <ul className="flex gap-8 text-white">
             <li>
-              <a href="" className="">Movies</a>
+              <a href="" className="">
+                Movies
+              </a>
             </li>
             <li>
-              <a href="" className=" ">Movies</a>
+              <a href="" className=" ">
+                Movies
+              </a>
             </li>
             <li>
-              <a href="" className=" ">Movies</a>
+              <a href="" className=" ">
+                Movies
+              </a>
             </li>
           </ul>
         </nav>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 relative">
           <div className="rounded-full bg-slate-200 p-2 ">
-            <img className="w-8" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png" alt="" />
+            <img
+              className="w-8"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png"
+              alt=""
+            />
           </div>
-          <span className="text-white">\/</span>
+          <button
+            id="userMenu"
+            className="text-white cursor-pointer"
+            onClick={() => {
+              setHandleUserMenu(!handleUserMenu);
+            }}
+          >
+            {handleUserMenu ? <ChevronUp /> : <ChevronDown />}
+          </button>
+          {handleUserMenu && (
+            <div className="absolute top-15 right-0 bg-slate-600 rounded-2xl py-4 px-8">
+              <ul className="">
+                <li className="font-semibold text-lg text-slate-200">
+                  <a href="">Movies</a>
+                </li>
+                <li className="font-semibold text-lg text-slate-200">
+                  <a href="">Categories</a>
+                </li>
+                <li className="font-semibold text-lg text-slate-200">
+                  <a href="">Streaming</a>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </header>
