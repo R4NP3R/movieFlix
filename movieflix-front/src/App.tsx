6fecea027b1ce7197d2abb63d5ccd745179405d4
R4NP3R <ssrafa.2014@gmail.com>
2026-01-05 18:43:37 -0300

    refactor: atualiza o app para receber componentes de cada rota

--- movieflix-front/src/App.tsx
+++ movieflix-front/src/App.tsx
@@ -1,13 +1,17 @@
-import { Header } from "./components/header"
-import { HomePage } from "./pages/homePage"
+import { Outlet } from "react-router";
+import { Header } from "./components/header";
 
 function App() {
   return (
     <>
-      <Header/>
-      <HomePage/>
+      <Header />
+      <main className="min-h-dvh bg-linear-to-b from-slate-900 to-20% to-slate-950">
+        <section className="pt-20 my-container">
+          <Outlet />
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
