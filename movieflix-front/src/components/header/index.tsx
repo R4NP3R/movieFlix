f15311f47e50432565e50633135524b27b814543
R4NP3R <ssrafa.2014@gmail.com>
2026-01-05 18:45:29 -0300

    feat: adiciona redirecionamento pelo react-dom e função para fechar o menu quando clicar fora dele

--- movieflix-front/src/components/header/index.tsx
+++ movieflix-front/src/components/header/index.tsx
@@ -1,13 +1,34 @@
-import { ChevronDown, ChevronUp } from "lucide-react";
-import { useState } from "react";
+import { ChevronDown, ChevronUp, User2Icon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router";
 
 export const Header = () => {
   const [handleUserMenu, setHandleUserMenu] = useState(false);
+  const userMenuRef = useRef<HTMLDivElement>(null);
+
+  function closeUserMenu() {
+    setHandleUserMenu(false)
+  }
+
+  useEffect(() => {
+    function clickOutsideUserMenu(e: MouseEvent) {
+      if(userMenuRef.current && !userMenuRef.current.contains(e.target as Node)){
+        closeUserMenu()
+      }
+    }
+    document.addEventListener("click", clickOutsideUserMenu)
+
+    return () => {
+      document.removeEventListener("click", clickOutsideUserMenu)
+    }
+
+  }, [])
+
 
   return (
     <header className="w-full fixed mt-">
       <div className="flex justify-between items-center my-container">
-        <h1 className="text-red-700 text-4xl font-bold">MovieFlix</h1>
+        <Link to="" className="text-red-700 text-4xl font-bold">MovieFlix</Link>
         <nav className="py-6 px-8 bg-slate-800 rounded-b-4xl">
           <ul className="flex gap-8 text-white">
             <li>
@@ -29,26 +50,25 @@ export const Header = () => {
         </nav>
         <div className="flex items-center gap-4 relative">
           <div className="rounded-full bg-slate-200 p-2 ">
-            <img
-              className="w-8"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1200px-User-avatar.svg.png"
-              alt=""
-            />
+            <User2Icon className="w-8 h-8"/>
           </div>
           <button
             id="userMenu"
             className="text-white cursor-pointer"
-            onClick={() => {
+            onClick={(e) => {
+              e.stopPropagation();
               setHandleUserMenu(!handleUserMenu);
             }}
           >
             {handleUserMenu ? <ChevronUp /> : <ChevronDown />}
           </button>
           {handleUserMenu && (
-            <div className="absolute top-15 right-0 bg-slate-600 rounded-2xl py-4 px-8">
-              <ul className="">
+            <div ref={userMenuRef} className="absolute top-15 right-0 bg-slate-600 rounded-2xl pb-4 ">
+              <h3 className="text-white font-bold border-b border-slate-400 text-center text-lg py-2">Create</h3>
+              <div className="px-8 pt-2">
+                <ul className="">
                 <li className="font-semibold text-lg text-slate-200">
-                  <a href="">Movies</a>
+                  <Link to="createMovie" onClick={closeUserMenu}>Movies</Link>
                 </li>
                 <li className="font-semibold text-lg text-slate-200">
                   <a href="">Categories</a>
@@ -57,6 +77,7 @@ export const Header = () => {
                   <a href="">Streaming</a>
                 </li>
               </ul>
+              </div>
             </div>
           )}
         </div>
