5faa07c9a98dcd49923a605bf4aec178f3bc0e31
R4NP3R <ssrafa.2014@gmail.com>
2026-01-08 15:44:03 -0300

    refactor: deixa o campo de releaseDate obrigatório

--- movieflix-front/src/zodSchemas/registerMovieSchema.ts
+++ movieflix-front/src/zodSchemas/registerMovieSchema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const registerMovieSchema = z.object({
   title: z.string().nonempty("Insert movie title"),
   description: z.string().nonempty("Insert movie description"),
-  releaseDate: z.string(),
+  releaseDate: z.string().nonempty("Insert movie release Date"),
   rating: z.string().nonempty("Insert the movie rating"),
   imageUrl: z.string().nonempty("Insert movie image url"),
   categories: z.array(z.number()).nonempty("Select at least one category"),
